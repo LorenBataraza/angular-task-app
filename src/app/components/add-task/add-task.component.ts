84245451854f71ae4bb5edacb45f754338a036f7
Lorenzo Cabrera Blanch <lorenzocabrerablanch@gmail.com>
2022-07-18 15:12:18 -0300

    Validating Method Not Working

--- src/app/components/add-task/add-task.component.ts
+++ src/app/components/add-task/add-task.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Output, EventEmitter} from '@angular/core';
 import { Subscription } from 'rxjs';
 import { UiService } from '../../service/ui.service';
 import { Task } from '../../Task';
+import { FormGroup, FormBuilder, Validators} from '@angular/forms';
 
 @Component({
   selector: 'app-add-task',
@@ -10,34 +11,66 @@ import { Task } from '../../Task';
 })
 export class AddTaskComponent implements OnInit {
   @Output() onAddTask: EventEmitter<Task> = new EventEmitter();
+  form: FormGroup;
 
-  text:string = "";
-  day:string = "";
-  reminder:boolean = false;
   showAddTask: boolean = false;
   subscription?: Subscription;
   
   constructor(
-    private uiService : UiService
+    private uiService : UiService,
+    private formBuilder: FormBuilder
     ) { 
       this.subscription = this.uiService.onToggle()
       .subscribe(
         value => this.showAddTask = value
-      )}
+      )
+
+      this.form = this.formBuilder.group({
+        text:['',[Validators.required]],
+        day:['', [Validators.required]],
+        reminder:[false],
+     })
+    }
 
   ngOnInit(): void {
   } 
 
-  onSubmit(){
-    if(this.text.length === 0){
-      alert("Please Add Text");
-      return
-    }
-    const {text,day,reminder} = this;
-    const newTask: Task = {text,day,reminder};
-    this.onAddTask.emit(newTask);
+
+  get InputText(){
+    return this.form.get("text");
+   }
+
+  get InputDate(){
+    return this.form.get("date");
   }
 
-// Todo Add Validators 
+  get InputReminder(){
+    return this.form.get("reminder");
+  }
 
+
+  onSubmit(event : Event){
+    event.preventDefault;
+
+    if(this.form.valid){
+
+      const newTask: Task = {
+        text: this.InputText,
+        date: this.InputDate,
+        reminder: this.InputReminder
+
+      };
+
+      this.onAddTask.emit(newTask);
+
+    }
+
+    // if(this.text.length === 0){
+    //   alert("Please Add Text");
+    //   return
+    // }
+
+  }
+
+  // Parse Natural Language String To a Date 
 }
